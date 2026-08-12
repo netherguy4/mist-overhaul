c42578e57dccb347aac8b55e5dcc8a91ecf247a9
NL <nl@develup.pro>
2026-08-12 20:34:32 +0300

    тест: проверка статичного портрета не зависит от blob-адреса

--- mist-overhaul.user.js
+++ mist-overhaul.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mist Overhaul
 // @namespace    https://github.com/netherguy4/mist-overhaul
-// @version      2026.08.12.2031
+// @version      2026.08.12.2034
 // @description  Анимированные портреты персонажей в Mist
 // @author       nether
 // @match        *://*.mist-game.ru/*
