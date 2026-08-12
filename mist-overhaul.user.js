0974f8c7cd646bd80d1c0a809197d5f44f6b174f
NL <nl@develup.pro>
2026-08-12 17:13:49 +0300

    README: описание инвалидации по хешу

--- mist-overhaul.user.js
+++ mist-overhaul.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mist Overhaul
 // @namespace    https://github.com/netherguy4/mist-overhaul
-// @version      2026.08.12.1710
+// @version      2026.08.12.1713
 // @description  Анимированные портреты персонажей в Mist
 // @author       nether
 // @match        *://*.mist-game.ru/*
